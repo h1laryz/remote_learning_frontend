1122aa851ad104747bdba4fe9200a05c7506ee95
Oleksii Demchenko <oleksii.demchenko@dopaminelabs.ltd>
2024-05-17 13:29:41 +0300

    changed navigation sequence

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -6,11 +6,11 @@ const Navigation = ({ jwtToken }) => {
   return (
     <div className="navigation">
       <Link to="/" className="navigation-link">Chat</Link>
-      <Link to="/login" className="navigation-link">Login</Link>
-      <Link to="/signup" className="navigation-link">Sign Up</Link>
       {jwtToken && <Link to="/admin" className="navigation-link">Admin</Link>}
       <Link to="/diary" className="navigation-link">Diary</Link>
       <Link to="/assignments" className="navigation-link">Assignments</Link>
+      <Link to="/login" className="navigation-link">Login</Link>
+      <Link to="/signup" className="navigation-link">Sign Up</Link>
     </div>
   );
 }
